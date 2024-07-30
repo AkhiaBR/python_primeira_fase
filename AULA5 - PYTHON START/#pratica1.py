572f83a44e64e80a8decc25518efec0c2a061e72
Fernando <65679737+AkhiaBR@users.noreply.github.com>
2024-07-29 21:39:24 -0300

    Delete AULA5 - PYTHON START/#pratica1.py

--- AULA5 - PYTHON START/#pratica1.py
+++
@@ -1,9 +0,0 @@
-#pratica1
-
-nome = input("Qual seu nome: ")
-idade = int(input("Digite sua idade: "))
-altura = int(input("Digite sua altura: "))
-
-print("Seu nome é: ", nome)
-print("Sua idade é: ", idade)
-print("Sua altura é: ", altura)
