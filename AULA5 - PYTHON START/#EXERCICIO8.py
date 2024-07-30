29c26360a45112f635632addc4d1c3fdc788224d
Fernando <65679737+AkhiaBR@users.noreply.github.com>
2024-07-29 22:02:45 -0300

    Add files via upload

--- AULA5 - PYTHON START/#EXERCICIO8.py
+++
@@ -1,8 +0,0 @@
-#EXERCICIO9
-
-nome = str(input("Digite seu nome: "))
-carga = int(input("Digite a sua carga horaria: "))
-valor = float(input("Digite o valor ganho por hora: "))
-bruto = (carga*valor)
-final = (bruto-(bruto*2/100))
-print(nome, "o salário final com desconto de INSS é de: ", final)
